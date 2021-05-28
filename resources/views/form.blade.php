18a32a746d7d38abd48fcaddb08479f053578239
no07 <41499440+no07@users.noreply.github.com>
2021-05-28 13:16:55 +0700

    update

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -5,21 +5,19 @@
     <meta name="_token" content="{{csrf_token()}}"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.4.0/min/dropzone.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/css/lightbox.css" integrity="sha512-Woz+DqWYJ51bpVk5Fv0yES/edIMXjj3Ynda+KWTIkGoynAMHrqTcDUQltbipuiaD5ymEo9520lyoVOo9jCQOCA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.4.0/dropzone.js"></script>
-</head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/js/lightbox.min.js" integrity="sha512-k2GFCTbp9rQU412BStrcD/rlwv1PYec9SNrkbQlo6RZCf75l6KcC3UwDY8H5n5hl4v77IDtIPwOk9Dqjs/mMBQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script></head>
 <body style="background-color: #2B2E33"><div class="container">
     <div class="row justify-content-center">
             <div class="col-md-12">
-
-                    <ul>
-                        @foreach ($images as $image)
-                            <li style="width:120px;display:inline-block;margin:5px 0px">
-                                <img src="{{ asset('images/' . $image->getFilename()) }}" width="120" height="120">
-                            </li>
-                        @endforeach
-                    </ul>
-
+                <ul>
+                    @foreach ($images as $image)
+                        <li style="width:120px;display:inline-block;margin:5px 0px">
+                            <a href="{{ asset('images/' . $image->getFilename()) }}" data-lightbox="image-{{ asset('images/' . $image->getFilename()) }}" data-title="My caption"><img src="{{ asset('images/' . $image->getFilename()) }}" width="120" height="120"></a>
+                        </li>
+                    @endforeach
+                </ul>
             </div>
         </div>
     </div>
